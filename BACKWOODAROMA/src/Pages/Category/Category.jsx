172d128eba4f0447d2452fff3da924126bc74802
SelnoxInfotech <117904460+SelnoxInfotech@users.noreply.github.com>
2022-12-28 17:57:53 +0530

    Add files via upload

---
+++ BACKWOODAROMA/src/Pages/Category/Category.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import Cookies from 'universal-cookie';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import Categorypopup from './Categorypopup';
+import axios from "axios"
+import { ThemeProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
+export default function Category() {
+    const CustomFontTheme = createTheme({
+        typography: {
+            fontSize: 25
+        },
+        components: {
+            MuiContainer: {
+                styleOverrides: {
+                    root: {
+                        fontSize: 24
+                    }
+                }
+            }
+        }
+    });
+   
+    const [totel , setTotal] = React.useState([])
+    React.useEffect(() => {
+        const cookies = new Cookies();
+        const token_data = cookies.get('Token_access')
+        axios("http://34.201.114.126:8000/AdminPanel/Get-Category/" ,{
+            
+        headers: {
+            'Authorization': `Bearer ${token_data}`
+            
+            
+          }
+        }).then(response => {
+            console.log(response)
+            setTotal(response.data)  
+        
+                
+        })
+    },[setTotal])
+
+    const columns = [
+        { field: 'name', headerName: 'Name',  editable: true,width: 180,  headerClassName: 'super-app-theme--header'},
+        { field: 'Status', headerName: 'Status', type: 'number', editable: true ,headerClassName: 'super-app-theme--header' },
+  
+    ];
+
+    const rows = totel
+
+    return (
+        <div className='container-fluid '>
+            <div className='row'>
+
+                <div className='col-sm-2 border  '>
+                   
+                </div>
+                <div className='col-8 border   ' >
+
+                    <div className='col-12 Add_Category'>
+                        <div className="col"> <h2>  Category
+                        </h2></div>
+                        <div className="col cat_but " >   <span className='btn'>{<Categorypopup></Categorypopup>}</span></div>
+                    </div>
+
+                    <div className='col-12'>
+                    <ThemeProvider theme={CustomFontTheme}>
+                        <div style={{ height: 400, width: '100%' }}>
+                            <DataGrid rows={rows} columns={columns} components={{ Toolbar: GridToolbar }}/>
+                        </div>
+                        </ThemeProvider>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+        </div>
+
+
+    )
+}
---
+++ BACKWOODAROMA/src/Pages/Tax/Tax.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import Cookies from 'universal-cookie';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { ThemeProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
+import axios from "axios";
+import Taxpop from "./Taxpopup";
+
+
+
+
+
+export default function Tax() {
+    const CustomFontTheme = createTheme({
+        typography: {
+            fontSize: 25,
+            colors : "#31B665"
+        },
+        components: {
+            MuiContainer: {
+                styleOverrides: {
+                    root: {
+                        
+                        fontSize: 24
+                    }
+                }
+            }
+        }
+    });
+
+    const cookies = new Cookies();
+    const token_data = cookies.get('Token_access')
+    const [totel, setTotal] = React.useState([])
+    React.useEffect(() => {
+        axios("http://34.201.114.126:8000/AdminPanel/Get-Tax/", {
+
+            headers: {
+                'Authorization': `Bearer ${token_data}`
+            }
+
+        }).then(response => {
+            setTotal(response.data)
+            console.log(response.data)
+                
+        })
+    }, [token_data])
+
+
+    const columns = [
+        { field: 'tax_value', headerName: 'Tax Value', width: 200, editable: true, headerClassName: 'super-app-theme--header' },
+        { field: 'tax_type', headerName: 'Tax Type', width: 200, editable: true, headerClassName: 'super-app-theme--header' },
+        { field: 'Status', headerName: 'Status', type: 'text', editable: true, width: 300, headerClassName: 'super-app-theme--header' },
+        { field: 'Edit', headerName: 'Edit', type: 'button', editable: true, headerClassName: 'super-app-theme--header' },
+        
+
+    ];
+
+    const rows = totel
+    return (
+        <div className='container-fluid'>
+            <div className='row'>
+
+                <div className='col-sm-2 '>
+
+                </div>
+                <div className='col-8 border   ' >
+
+                    <div className='col-12 Add_Category'>
+                        <div className="col"> <h2> Tax
+                        </h2></div>
+                        <div className="col cat_but" >  <span className='btn'> <h2> <Taxpop></Taxpop> </h2></span></div>
+                    </div>
+
+                    <div className='col-12' >
+                        <ThemeProvider theme={CustomFontTheme}>
+                            <div style={{ height: 400, width: '100%', }}>
+                                <DataGrid rows={rows} columns={columns}  components={{ Toolbar: GridToolbar }}  checkboxSelection/>
+                            </div>
+                        </ThemeProvider>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+        </div>
+
+
+    )
+}
